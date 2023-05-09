b2cc7fda73ff83fc9f39b6a5c69f1afda4decf8c
synphonics <44014224+synphonics@users.noreply.github.com>
2023-05-09 09:03:15 -0400

    updated

--- model.py
+++ model.py
@@ -2,22 +2,21 @@ import pandas as pd
 import numpy as np
 import os
 import re
+import pickle
 
 from sklearn.model_selection import train_test_split
-
-
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
-
 from sklearn import metrics
 from sklearn.feature_selection import SelectFromModel
-
 import lightgbm as lgbm
 
-def create_model():
-    data = pd.read_csv(os.getcwd()+"\\data\\data.csv")
+from feature_engineering import feature_engineering
 
-    x, y = data.drop(['soldPrice', 'listPrice'], axis = 1), data['soldPrice']
+def create_model(days, type_ = None):
+    data = feature_engineering(days, type_)
+
+    x, y = data.drop(['soldPrice'], axis = 1), data['soldPrice']
 
     x = x.rename(columns = lambda z: re.sub('[^A-Za-z0-9_]+', '', z))
     
@@ -39,13 +38,6 @@ def create_model():
     #Rename the columns of training and test sets to include column names of top 100 features
     train_x = pd.DataFrame(x_train, columns = x_train.columns[feat_index])
     test_x = pd.DataFrame(x_test, columns= x_test.columns[feat_index])
-
-
-    #Print the top 100 features
-    for col in x_train.columns[feat_index]:
-        print(col)
-
-    lgbm.plot_importance(base_regressor.fit(x_train,y_train), max_num_features = 25)
     
     #LightGBM parameters
     params = {
@@ -66,8 +58,7 @@ def create_model():
     #Get results of gridsearch 
     grid_results_df = pd.DataFrame(grid_search.cv_results_)
     #Save to csv
-    grid_results_df.to_csv("GridSearch_CV_results.csv", index = False)
-    grid_results_df
+
     grid_results_df.sort_values(by=['rank_test_score']).head()
     best_model = lgbm.LGBMRegressor(n_estimators=100, device = 'gpu')
     best_model.set_params(**grid_results_df.sort_values(by=['rank_test_score'])['params'].values[0])
@@ -78,3 +69,16 @@ def create_model():
     y_pred = best_model.predict(test_x)
     metrics.mean_absolute_error(y_test, y_pred)
     metrics.r2_score(y_test, y_pred)
+
+    model_parameters = {"model": best_model, "columns": x_train.columns, "idx": feat_index}
+    filename = f"{type_}_model_parameters.sav"
+    with open(os.getcwd()+"\\models", 'wb') as f:
+        pickle.dump(model_parameters, open(filename, 'wb'))
+
+
+def main(days, type_):
+    if type_:
+        create_model(days, type_)
+    else:
+        create_model(days, 'sale')
+        create_model(days, 'lease')
