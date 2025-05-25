744a7286cb75aca3ab1eafe0badf79e6ef520399
julian fong <cyfongj@gmail.com>
2025-05-24 20:47:56 -0400

    updates

--- real_estate_predictor/config/__init__.py
+++ real_estate_predictor/config/__init__.py
@@ -2,4 +2,8 @@ from .config import FEATURE_ENGINEERING_FILE  # noqa: F401
 from .config import LISTING_PARAMETERS  # noqa: F401
 from .config import NEIGHBOURHOOD_KEYS  # noqa: F401
 from .config import NEIGHBOURHOODS  # noqa: F401
-from .config import DATACLEANER_FILE, NEIGHBOURHOOD_PARAMETERS, PREPROCESSOR_FILE # noqa: F401
+from .config import (
+    DATACLEANER_FILE,
+    NEIGHBOURHOOD_PARAMETERS,
+    PREPROCESSOR_FILE,
+)  # noqa: F401
