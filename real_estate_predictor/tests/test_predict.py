cda5a5297028c148469056937f704dc0ba087934
julian fong <cyfongj@gmail.com>
2025-07-01 12:06:16 -0400

    update test_predict.py

--- real_estate_predictor/tests/test_predict.py
+++ real_estate_predictor/tests/test_predict.py
@@ -1,7 +1,9 @@
 """Test file to generate predictions."""
-from real_estate_predictor.predict import predict_sale_listing
+
+from real_estate_predictor.predict import extract_input, predict, predict_sale_listing
 
 mlsNumber = "N12174739"
+
 
 def test_sale_listing_prediction():
     """Test sale listing prediction."""
@@ -11,3 +13,9 @@ def test_sale_listing_prediction():
     assert "mlsNumber" in prediction
     assert "prediction" in prediction
     assert prediction["mlsNumber"] == mlsNumber
+
+
+def test_predict_with_sale():
+    mlsNumber, data = extract_input(mlsNumber)
+    prediction = predict(data, "sale")[0].item()
+    return {"mlsNumber": mlsNumber, "prediction": prediction}
