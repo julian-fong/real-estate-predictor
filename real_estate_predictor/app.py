da043eb2f338e2841bfcf12e1512ad30fa6c4f6c
julian fong <cyfongj@gmail.com>
2025-07-01 13:17:05 -0400

    fixed cors

--- real_estate_predictor/app.py
+++ real_estate_predictor/app.py
@@ -6,6 +6,25 @@ app = FastAPI()
 
 # fastapi dev app.py
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://landpower.ca",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
